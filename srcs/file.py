04aec579a861d1ed4b8c6c81e4c0b5e51c12062d
Gauthier <gauthier.pladet@gmail.com>
2021-07-01 15:46:38 +0200

    Verifie si le repertoire du fichier csv existe ou pas sinon creation auto du dossier

--- srcs/file.py
+++ srcs/file.py
@@ -1,11 +1,14 @@
 import csv
+import os
 
 header = ['product_page_url', 'universal_product_code (upc)', 'title', 'price_including_tax', 'price_excluding_tax',
           'number_available', 'product_description', 'category', 'review_rating', 'image_url']
 
 
 # Function who creates a csv file and writes header and data in columns
-def create_file_csv(filename: str, fields: list):
+def create_file_csv(directory: str, filename: str, fields: list):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     with open(filename, 'w', encoding='utf-8-sig') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(header)
