131dcf48ff5bb93b924c825afeb4b95151154373
Gauthier <gauthier.pladet@gmail.com>
2021-07-02 15:42:18 +0200

    enregistrement de tous les livres d une categorie dans un fichier csv

--- srcs/category.py
+++ srcs/category.py
@@ -17,38 +17,45 @@ def get_url_books(soup: BeautifulSoup):
 
 
 if __name__ == '__main__':
-    i = 1
     url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/'\
         'page-1.html'
     url_books = []
     filename = "files/category.csv"
-    response = requests.get(url)
-    if response.ok:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        url_books = get_url_books(soup)
-        index = 0
-        for element in url_books:
-            if response.ok:
-                data = []
-                dictionary = {}
-                response = requests.get(element)
-                soup = BeautifulSoup(response.content, 'html.parser')
-                data.append(url)
-                dictionary = scrape.get_product_info(soup)
-                data.append(scrape.get_upc(dictionary))
-                data.append(scrape.get_title(soup))
-                data.append(scrape.get_price_excluding_tva(dictionary))
-                data.append(scrape.get_price_including_tva(dictionary))
-                data.append(scrape.get_available(dictionary))
-                data.append(scrape.get_product_description(soup))
-                data.append(scrape.get_category(soup))
-                data.append(scrape.get_rating(dictionary))
-                data.append(scrape.get_url_img(soup))
-                if (index == 0):
-                    file.create_file_csv('files', filename, data)
-                else:
-                    file.append_file_csv(filename, data)
-                index += 1
-        print("Data saved in the following csv file : " + filename, end="")
-    else:
-        print('Error: Url not found', end='')
+    next_page = True
+    i = 0
+    j = 1
+    while (next_page):
+        if (i != 0):
+            url = url.replace('page-' + str(i), 'page-' + str(j))
+        response = requests.get(url)
+        if response.ok:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            url_books = get_url_books(soup)
+            index = 0
+            for element in url_books:
+                if response.ok:
+                    data = []
+                    dictionary = {}
+                    response = requests.get(element)
+                    soup = BeautifulSoup(response.content, 'html.parser')
+                    data.append(url)
+                    dictionary = scrape.get_product_info(soup)
+                    data.append(scrape.get_upc(dictionary))
+                    data.append(scrape.get_title(soup))
+                    data.append(scrape.get_price_excluding_tva(dictionary))
+                    data.append(scrape.get_price_including_tva(dictionary))
+                    data.append(scrape.get_available(dictionary))
+                    data.append(scrape.get_product_description(soup))
+                    data.append(scrape.get_category(soup))
+                    data.append(scrape.get_rating(dictionary))
+                    data.append(scrape.get_url_img(soup))
+                    if (index == 0 and i == 0):
+                        file.create_file_csv('files', filename, data)
+                    else:
+                        file.append_file_csv(filename, data)
+                    index += 1
+            i += 1
+            j += 1
+        else:
+            next_page = False
+    print("Data saved in the following csv file : " + filename, end="")
